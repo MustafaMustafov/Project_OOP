86a3a8bbe5a1d5845e643c919283db055edb1205
DKichukov <dzhamal.kichukov@gmail.com>
2022-11-02 20:59:48 +0200

    add Table class

---
+++ src/Table.java
@@ -0,0 +1,9 @@
+public class Table {
+    private int tableNumber;
+    private String status;
+
+    public Table(int tableNumber, String status) {
+        this.tableNumber = tableNumber;
+        this.status = status;
+    }
+}
