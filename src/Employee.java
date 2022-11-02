f5b3641813fea7fbe6f54f829933a7dece5f18b8
DKichukov <dzhamal.kichukov@gmail.com>
2022-11-02 20:43:08 +0200

    add Employee class

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String name;
+    private String userName;
+    private String password;
+
+    public Employee(String name, String userName, String password) {
+        this.name = name;
+        this.userName = userName ;
+        this.password = password;
+    }
+}
