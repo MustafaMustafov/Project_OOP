7b9c0ed041038be6eaa617a46116d17a897c8e5a
MustafaMustafov <musitoahmed@gmail.com>
2022-11-06 16:21:14 +0200

    Added methods to class Waiter Added new class FoodMenu

--- src/Restaurant/Drink.java
+++ src/Restaurant/Drink.java
@@ -1,28 +1,9 @@
 package Restaurant;
 
-public class Drink extends Meal {
-    private boolean isDrinkHot;
-    private int size;
+public class Drink extends Food {
 
-    public Drink(String name, double price, boolean isServed, boolean isDrinkHot, int size) {
-        super(name, price, isServed);
-        this.isDrinkHot = isDrinkHot;
-        this.size = size;
-    }
 
-    public boolean isDrinkHot() {
-        return isDrinkHot;
-    }
-
-    public void setDrinkHot(boolean drinkHot) {
-        isDrinkHot = drinkHot;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
+    public Drink(String name, double price) {
+        super(name, price);
     }
 }
--- src/Staff/Waiter.java
+++ src/Staff/Waiter.java
@@ -1,8 +1,44 @@
 package Staff;
 
-public class Waiter extends Employee {
+import Restaurant.Food;
+import Restaurant.Meal;
+import Restaurant.FoodMenu;
 
+import java.util.Scanner;
+
+public class Waiter extends Employee {
+    public static Scanner scan = new Scanner(System.in);
     public Waiter(String name, String userName, String password) {
         super(name, userName, password);
     }
+
+    public void addMealToMenu() {
+        System.out.println("Enter meal name: ");
+        String mealName = scan.nextLine();
+        System.out.println("Enter meal price: ");
+        double mealPrice = scan.nextDouble();
+        System.out.println("Enter meal type: ");
+        String mealType = scan.nextLine();
+        FoodMenu.meals.add(new Meal(mealName,mealPrice,mealType,false));
+    }
+
+    public void removeMealFromMenu() {
+        System.out.println("Enter meal name to remove from menu: ");
+        String mealName = scan.nextLine();
+        for (int i = 0; i < FoodMenu.meals.size(); i++) {
+            if (FoodMenu.meals.get(i).getName().equals(mealName)) {
+                FoodMenu.meals.remove(FoodMenu.meals.get(i));
+            }
+        }
+    }
+
+    public void displayMenu() {
+        System.out.println(" ====== Menu ====== ");
+        System.out.println(" -- Name -- Price -- Type -- ");
+        for (int i = 0; i < FoodMenu.meals.size(); i++) {
+            System.out.println(FoodMenu.meals.get(i).getName() + " - " + FoodMenu.meals.get(i).getPrice() + " - " + FoodMenu.meals.get(i).getType());
+        }
+    }
+
 }
+
