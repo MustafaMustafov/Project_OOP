743b9473b529be7e730783b15faf917ea360e423
DKichukov <dzhamal.kichukov@gmail.com>
2022-11-07 16:22:35 +0200

    Meal and Drink menu lists creation.

--- src/Restaurant/Drink.java
+++ src/Restaurant/Drink.java
@@ -6,4 +6,9 @@ public class Drink extends Food {
     public Drink(String name, double price) {
         super(name, price);
     }
+
+    @Override
+    public String toString() {
+        return getName() + "," + getPrice();
+    }
 }
--- src/Restaurant/FoodMenu.java
+++ src/Restaurant/FoodMenu.java
@@ -3,6 +3,4 @@ import java.util.ArrayList;
 
 public abstract class FoodMenu {
     public static ArrayList<Meal> meals;
-
-
 }
