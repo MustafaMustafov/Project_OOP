ccdbc194af5c83dfb2adc8033eba15e6d218822e
DKichukov <dzhamal.kichukov@gmail.com>
2022-11-12 20:13:04 +0200

    conflict repairing files with changes

--- src/Management/ObjectFileManagement.java
+++ src/Management/ObjectFileManagement.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectFileManagement {
-    //TODO Object class needs to be Serializable
+
     public static<T> void writeObjectToFile(ArrayList<T> objects, String inputName) {
         try {
-            String fileName = "./src/ProgramFiles/" + inputName;
+            String fileName = "project/src/ProgramFiles/" + inputName;
             FileOutputStream outputStream = new FileOutputStream(fileName);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
             objectOutputStream.writeObject(objects);
@@ -21,7 +21,7 @@ public class ObjectFileManagement {
     public static<T> ArrayList<T> readObjectFromFile(String inputName) {
         ArrayList<T> obj = new ArrayList<>();
         try {
-            String fileName = "src/ProgramFiles/" + inputName;
+            String fileName = "project/src/ProgramFiles/" + inputName;
             FileInputStream inputStream = new FileInputStream(fileName);
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
             obj = (ArrayList<T>) objectInputStream.readObject();
--- src/RMS.java
+++ src/RMS.java
@@ -1,22 +1,111 @@
 import Management.ObjectFileManagement;
 import Management.UserManagement;
+import Staff.Chef;
 import Staff.Employee;
+import Staff.Waiter;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import static Management.UserManagement.checkPosition;
+import static Management.UserManagement.loginUser;
 
 public class RMS {
     static int select;
+    static Scanner sc = new Scanner(System.in);
     static ArrayList<Employee> staff = ObjectFileManagement.readObjectFromFile("users.csv");
 
 
     public static void main(String[] args) {
-            System.out.println("Welcome to RMS!");
+        System.out.println("Welcome to RMS!");
+        do {
+            UserManagement.loginUser(staff);
+            boolean key = checkPosition();
+            if (key) {
+                runMenuWaiter();
+            } else {
+                runMenuChef();
+            }
 
-            do {
-                UserManagement.loginUser(staff);
-
-
-            } while (select != 0);
-        }
+        } while (select != 0);
     }
+
+    public static void runMenuWaiter() {
+        Waiter waiter = new Waiter();
+        do {
+            System.out.println("-------Waiter MENU -------");
+            System.out.println("1.ADD new meal to MENU");
+            System.out.println("2.REMOVE meal from MENU");
+            System.out.println("3.DISPLAY food menu");
+            System.out.println("4.DISPLAY active orders");
+            System.out.println("5.CREATE new order");
+            System.out.println("6.EDIT order");
+            System.out.println("7.CHANGE order status");
+            System.out.println("8.Logout account");
+            System.out.println("0.EXIT");
+            System.out.println("----------------------------");
+            System.out.println("Select: ");
+            select = sc.nextInt();
+            switch (select) {
+                case 0:
+                    System.out.println("The program is ending!");
+                    return;
+                case 1:
+                    waiter.addMealToMenu();
+                case 2:
+                    waiter.removeMealFromMenu();
+                    break;
+                case 3:
+                    waiter.displayMenu();
+                    break;
+                case 4:
+                    waiter.displayActiveOrders();
+                    break;
+                case 5:
+                    waiter.inputNewOrder();
+                    break;
+                case 6:
+                    waiter.editOrder();
+                    break;
+                case 7:
+                    waiter.changeOrderStatus();
+                    break;
+                case 8:
+                    return;
+                default:
+                    System.out.println("Wrong input!!");
+                    break;
+            }
+        } while (select != 0);
+    }
+
+    public static void runMenuChef() {
+        Chef chef = new Chef();
+        do {
+            System.out.println("-------Chef MENU -------");
+            System.out.println("1.DISPLAY order");
+            System.out.println("2.UPDATE order status");
+            System.out.println("3.Logout account");
+            System.out.println("0.EXIT");
+            System.out.println("----------------------------");
+            System.out.println("Select: ");
+            select = sc.nextInt();
+            switch (select) {
+                case 0:
+                    System.out.println("The program is ending!");
+                    return;
+
+                case 1:
+                    chef.displayOrders();
+                    break;
+                case 2:
+                    chef.updateOrderStatus();
+                    break;
+                case 3:
+                    return;
+            }
+        }while(select!=0);
+
+    }
+}
 
