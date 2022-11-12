fc8e7a138de5361fd56a1f72f06b6ee947810797
MustafaMustafov <musitoahmed@gmail.com>
2022-11-12 22:31:35 +0200

    Added few methods to Waiter Menu

--- src/Restaurant/FoodMenu.java
+++ src/Restaurant/FoodMenu.java
@@ -3,8 +3,16 @@ package Restaurant;
 import java.util.ArrayList;
 
 public abstract class FoodMenu {
-    public static ArrayList<Meal> meals;
-    public static ArrayList<Drink> drinks;
+    public static ArrayList<Meal> meals = loadList("Meals.csv");
+    public static ArrayList<Drink> drinks = loadList("Drinks.csv");
+
+    public static ArrayList<Meal> getMeals() {
+        return meals;
+    }
+
+    public static ArrayList<Drink> getDrinks() {
+        return drinks;
+    }
 
     public static <T> ArrayList<T> loadList(String fileName) {
         ArrayList<T> newList = new ArrayList<>();
